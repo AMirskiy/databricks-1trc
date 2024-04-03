dbd68b5f79a8480b38bfda794313ef627db20299
Andrey Mirskiy <35451244+AMirskiy@users.noreply.github.com>
2024-04-03 10:02:18 +0200

    clean up

--- 1BRC partitioned.sql
+++
@@ -1,5 +0,0 @@
-SET use_cached_result=false;
-SELECT station, min(measure), max(measure), mean(measure)
-FROM `1brc`.`measurements_delta_partitioned`
-GROUP BY station
-ORDER BY station;
--- 1TRC non-partitioned.sql
+++
@@ -1,5 +0,0 @@
-SET use_cached_result=false;
-SELECT station, min(measure), max(measure), mean(measure)
-FROM `1trc`.`measurements_delta`
-GROUP BY station
-ORDER BY station;
